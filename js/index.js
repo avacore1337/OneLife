8f21680eeb577f0b210428a187384f3a9f56d4e9
Robert Welin-Berger <avacore1337@gmail.com>
2021-08-01 17:10:20 +0200

    switched vue from cdn to local

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+import Vue from 'vue/dist/vue.js';
 import("../pkg/index.js").then(function(wasm) {
 const hard_reset = document.getElementById("Hard Reset");
 hard_reset.addEventListener("click", event => {
 	console.log("hard reset");
 	wasm.hard_reset();
+	this.state = wasm.get_state();
 });
 const save = document.getElementById("Save");
 save.addEventListener("click", event => {
@@ -13,6 +15,7 @@ const load = document.getElementById("Load");
 load.addEventListener("click", event => {
 	console.log("load");
 	wasm.load();
+	this.state = wasm.get_state();
 });
 const world = wasm.get_world();
 console.log(world);
