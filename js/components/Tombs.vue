4936b5e27354b5ce63a36ad4e0fbe4825eb47deb
Robert Welin-Berger <avacore1337@gmail.com>
2021-12-22 00:25:57 +0100

    refactor to similar map/filter style

--- js/components/Tombs.vue
+++ js/components/Tombs.vue
@@ -3,15 +3,11 @@
     <span v-if="state.tombs.some((tomb) => tomb.is_purchased)">
       <h4>Bought Tombs</h4>
       <table>
-        <tr
-          v-for="(tomb, index) in world.tombs"
-          v-if="state.tombs[index].is_visible && state.tombs[index].is_purchased"
-          :key="tomb.name"
-        >
+        <tr v-for="[tomb, tomb_state] in bought_tombs" :key="tomb.name">
           <td>
             <span>{{ tomb.display_name }}</span>
             <br />
-            <span>Coins: {{ tomb.coin_gain }} </span>
+            <span>Coins: {{ tomb_state.effective_income }} </span>
           </td>
         </tr>
       </table>
@@ -20,16 +16,15 @@
     <h4>Tombs</h4>
     <table>
       <tr
-        v-for="(tomb, index) in world.tombs"
-        v-if="state.tombs[index].is_visible && !state.tombs[index].is_purchased"
-        v-bind:class="{ disabled: !state.tombs[index].is_unlocked }"
+        v-for="[tomb, tomb_state] in visible_unbought_tombs"
+        v-bind:class="{ disabled: !tomb_state.is_unlocked }"
         :key="tomb.name"
       >
         <td v-on:click="buyTomb(tomb.name)">
           <span>{{ tomb.display_name }} </span>
           <span style="float: right">Cost: {{ tomb.purchasing_cost }} money </span>
           <br />
-          <span>Coins: {{ state.tombs[index].effective_income }} </span>
+          <span>Coins: {{ tomb_state.effective_income }} </span>
         </td>
       </tr>
     </table>
@@ -42,6 +37,28 @@ import Section from "./Section.vue";
 export default {
   props: ["state", "world", "input", "wasm"],
   components: { Section },
+  computed: {
+    visible_unbought_tombs: function () {
+      let self = this;
+      return self.world.tombs
+        .map((w, i) => {
+          return [w, self.state.tombs[i]];
+        })
+        .filter(([w, s]) => {
+          return s.is_visible && !s.is_purchased;
+        });
+    },
+    bought_tombs: function () {
+      let self = this;
+      return self.world.tombs
+        .map((w, i) => {
+          return [w, self.state.tombs[i]];
+        })
+        .filter(([w, s]) => {
+          return s.is_purchased;
+        });
+    },
+  },
   methods: {
     buyTomb: function (tomb_name) {
       this.wasm.buy_tomb(tomb_name);
