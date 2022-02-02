21081f4453b37a9c7ac8b5b1e12380b16d31d356
Robert Welin-Berger <avacore1337@gmail.com>
2022-02-02 09:14:11 +0100

    switched to more flexbox

--- js/components/Main.vue
+++ js/components/Main.vue
@@ -1,55 +1,57 @@
 <template>
   <b-tabs>
     <b-tab title="Life" key="life-tab">
-      <div style="margin-left: 1%; float: left; width: 49%">
-        <Works
-          v-bind:metaData="metaData"
-          v-bind:state="state"
-          v-bind:input="input"
-          v-bind:world="world"
-          v-bind:wasm="wasm"
-        />
-        <Housing
-          v-bind:metaData="metaData"
-          v-bind:state="state"
-          v-bind:input="input"
-          v-bind:world="world"
-          v-bind:wasm="wasm"
-        />
-      </div>
-      <div style="margin-left: 1%; float: left; width: 49%">
-        <Activities v-bind:state="state" v-bind:input="input" v-bind:world="world" v-bind:wasm="wasm" />
-        <BoostItems
-          v-bind:state="state"
-          v-bind:input="input"
-          v-bind:world="world"
-          v-bind:wasm="wasm"
-          v-bind:metaData="metaData"
-          v-bind:item_queue="item_queue"
-        />
-        <Blessings
-          v-if="state.rebirth_stats.unlocks.has_faith"
-          v-bind:metaData="metaData"
-          v-bind:state="state"
-          v-bind:input="input"
-          v-bind:world="world"
-          v-bind:wasm="wasm"
-        />
-        <div v-if="metaData.options.show_recorded">
-          <RecordedInputs
-            title="Previous Recorded Inputs"
-            v-bind:recorded_inputs="previous_recorded_inputs"
+      <div class="my-container">
+        <div class="main-item">
+          <Works
+            v-bind:metaData="metaData"
+            v-bind:state="state"
+            v-bind:input="input"
+            v-bind:world="world"
             v-bind:wasm="wasm"
-            v-bind:remove_recorded="wasm.remove_previous_recorded"
-            v-bind:clear_recorded="wasm.clear_previous_recorded"
           />
-          <RecordedInputs
-            title="Current Inputs"
-            v-bind:recorded_inputs="recorded_inputs"
+          <Housing
+            v-bind:metaData="metaData"
+            v-bind:state="state"
+            v-bind:input="input"
+            v-bind:world="world"
             v-bind:wasm="wasm"
-            v-bind:clear_recorded="wasm.clear_recorded"
-            v-bind:remove_recorded="wasm.remove_recorded"
           />
+        </div>
+        <div class="main-item">
+          <Activities v-bind:state="state" v-bind:input="input" v-bind:world="world" v-bind:wasm="wasm" />
+          <BoostItems
+            v-bind:state="state"
+            v-bind:input="input"
+            v-bind:world="world"
+            v-bind:wasm="wasm"
+            v-bind:metaData="metaData"
+            v-bind:item_queue="item_queue"
+          />
+          <Blessings
+            v-if="state.rebirth_stats.unlocks.has_faith"
+            v-bind:metaData="metaData"
+            v-bind:state="state"
+            v-bind:input="input"
+            v-bind:world="world"
+            v-bind:wasm="wasm"
+          />
+          <div v-if="metaData.options.show_recorded">
+            <RecordedInputs
+              title="Previous Recorded Inputs"
+              v-bind:recorded_inputs="previous_recorded_inputs"
+              v-bind:wasm="wasm"
+              v-bind:remove_recorded="wasm.remove_previous_recorded"
+              v-bind:clear_recorded="wasm.clear_previous_recorded"
+            />
+            <RecordedInputs
+              title="Current Inputs"
+              v-bind:recorded_inputs="recorded_inputs"
+              v-bind:wasm="wasm"
+              v-bind:clear_recorded="wasm.clear_recorded"
+              v-bind:remove_recorded="wasm.remove_recorded"
+            />
+          </div>
         </div>
       </div>
     </b-tab>
@@ -127,5 +129,10 @@ export default {
 <style scoped>
 b-tab {
   color: white;
+}
+
+.main-item {
+  flex-grow: 1;
+  flex-basis: 0;
 }
 </style>
