9563a8070e88c8329969bb0e18d4c011a4020e22
Robert Welin-Berger <avacore1337@gmail.com>
2022-02-16 16:45:29 +0100

    fixed housing

--- js/components/Housing.vue
+++ js/components/Housing.vue
@@ -1,13 +1,12 @@
 <template>
-  <Section title="Housing">
-    <b-button
-      v-if="state.rebirth_stats.unlocks.can_auto_living"
-      size="sm"
-      @click="toggle_auto_living"
-    >
-      {{ !metaData.options.auto_living ? 'Auto Living' : "Don't Auto Living" }}
-    </b-button>
+  <Section2 title="Housing">
     <table>
+      <tr class="header-row">
+        <th>Housing</th>
+        <th>Happiness</th>
+        <th>Health</th>
+        <th>Cost</th>
+      </tr>
       <tr
         v-for="[housing, housing_state] in visible_housing"
         :key="housing.name"
@@ -15,31 +14,29 @@
         @click="wasm.set_housing(housing.name)"
       >
         <td>
-          <p>
-            <span :class="{ selected: input.housing === housing.name }"
-              >{{ housing.display_name }}
-            </span>
-            <span v-if="!housing_state.is_unlocked" style="float: right"
-              >Required Money: {{ housing.required_money }}
-            </span>
-            <span style="float: right">Cost: {{ housing.upkeep }}/s </span>
-            <br />
-            Happiness boost: {{ housing.happiness_factor }},
-            <span v-if="housing.health_effect != 0.0">
-              Health Effect: {{ housing.health_effect }}
-            </span>
-          </p>
+          <span :class="{ selected: input.housing === housing.name }"
+            >{{ housing.display_name }}
+          </span>
         </td>
+        <td v-if="!housing_state.is_unlocked">
+          Required Money: <FormatNumber :value="housing.required_money" />
+        </td>
+        <td v-if="housing_state.is_unlocked">{{ housing.happiness_factor }}X</td>
+        <td v-if="housing_state.is_unlocked">
+          {{ housing.health_effect }}
+        </td>
+        <td v-if="housing_state.is_unlocked"><FormatNumber :value="housing.upkeep" />/s</td>
       </tr>
     </table>
-  </Section>
+  </Section2>
 </template>
 
 <script>
-import Section from './Section.vue'
+import Section2 from './Section2.vue'
+import FormatNumber from './FormatNumber.vue'
 
 export default {
-  components: { Section },
+  components: { Section2, FormatNumber },
   props: ['metaData', 'state', 'world', 'input', 'wasm'],
   computed: {
     visible_housing() {
